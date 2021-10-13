624fb73ed56050258f0f849440941f974c30fcb3
jakubKuznik <kuznja16@gmail.com>
2021-10-13 11:33:41 +0200

    Add files via upload

--- xkuzni04.s
+++ xkuzni04.s
@@ -4,24 +4,17 @@
         .data 0x04          	; zacatek data segmentu v pameti
 login:  .asciiz "xkuzni04"  	; <-- nahradte vasim loginem
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; k = 11
-; u = 21 
 
 cipher: ;.space 9 		;sem ukladejte sifrovane znaky 
 				;(za posledni nezapomente dat 0)
         .align 2            	; dale zarovnavej na ctverice (2^2) bajtu
-
-
 
 laddr:  .word login         	; 4B adresa vstupniho textu (pro vypis)
 caddr:  .word cipher        	; 4B adresa sifrovaneho retezce (pro vypis)
 
 
         .text 0x40          	; adresa zacatku programu v pameti
-        .global main        	; 
-
-
+        .global main        	
 
 
 ;xkuzni04 -r15-r16-r19-r20-r23-r0
@@ -29,46 +22,105 @@ caddr:  .word cipher        	; 4B adresa sifrovaneho retezce (pro vypis)
 
 ; sem doplnte reseni Vernamovy sifry dle specifikace v zadani
 main:   
-	addi r16, r0, 11 	; index v abecede meho pismena k 
-	addi r19, r0, 21	; index v abecede meho pismena u
-	addi r15, r0, 64        ; asci value where alpha characters start
-		
-	;sgt  r3,r1,r2     ; if r1>r2 then r3=1 else r3 = 0
-        ;bnez r3, true     ; branch if r3 not equals zero
+	; a = 97 ASCI 
+	; z = 122 ASCI
+	; k = 11 ASCI	
+	; u = 21 ASCI
 
+
+	addi r19, r0, 21	; index v abecede meho pismena u
 
 ;---------------------------------------------------------------------
- ;loop:
-	lb r23, login+0(r20)	; načte bajt z adresy login index 0	
-				;if number 
+ loop:
+
+;!!!!!!!!!!!!!!!! LICHY INDEX !!!!!!!!!!!!!!!!!!!!!!!!!
+
+	lb r23, login(r20)	;načte bajt z adresy login index 0	
+
+;########## NUMBER CHECK   	; if number go to :number
 	addi r16, r0, 0
-	addi r19, r23, 0
-
-	sgt r19, r15, r16       ; porovna aktualni znak a zjisti jestli je pismeno nebo cislo
+	addi r15, r0, 97        ; asci value where alpha characters start 
+	sgt r16, r15, r23       ; porovna aktualni znak a zjisti jestli je pismeno nebo cislo
 	bnez r16, num		; pokud to je cislo skoci na navesti cislo
+;#########################
+	nop	
+	nop	
 	
-	addi r19, r0, 21	; vrati puvodni hodnotu 
-	addi r16, r0, 11        ; vrati puvodni hodnotu 	
-	
-	;nop	
-	;nop	
+;########### Check if asci overflow
+	add r19, r0, r23   	; store to temp variable becouse dont want to increment pointer
+	addi r19, r19, 11  	; increment asci value 
+	addi r16, r0, 122   	; 122 is max asci value 
+	addi r15, r0, 0	        ; condition register
+	sgt r15, r19, r16     	; check if asci value overflow value of z that is 122
+	bnez r15, overflow_over
+;######################
+	nop	
+	nop	
 
-	;todo shift over edges
-	add r19, r0, r23
-	add r19, r19, r16
-
-	sb cipher(r20), r19     ; uloží bajt do cipher na index 0
-	addi r19, r0, 21
-
+;##### ZAPIS HODNOTY A INKREMENT 
+	sb cipher(r20), r19     ; uloží bajt do cipher na index 
+continue:	
 	addi r23, r23, 1   	; pointer increment
 	addi r20, r20, 1   	; pointer increment
-	
-	;j loop	
-;---------------------------------------------------------------------
-	
+;#################################
 
-   num:
 	
+;!!!!!!!!!!!!!!!!!!Sudy index !!!!!!!!!!!!!!!!!!
+
+	lb r23, login(r20)	;načte bajt z adresy login index 0	
+
+;########## NUMBER CHECK   	; if number go to :number
+	addi r16, r0, 0
+	addi r15, r0, 97        ; asci value where alpha characters start 
+	sgt r16, r15, r23       ; porovna aktualni znak a zjisti jestli je pismeno nebo cislo
+	bnez r16, num		; pokud to je cislo skoci na navesti cislo
+;#########################
+	nop	
+	nop	
+
+;########### Check if asci underflow
+	add r19, r0, r23   	; store to temp variable becouse dont want to increment pointer
+	subi r19, r19, 21  	; decrement asci value 
+	addi r16, r0, 97   	; 97 is smallest asci value 
+	addi r15, r0, 0	        ; condition register
+	sgt r15, r16, r19    	; check if asci value underflow 97
+	bnez r15, overflow_under
+;######################
+	nop	
+	nop	
+
+;##### ZAPIS HODNOTY A INKREMENT 
+	sb cipher(r20), r19     ; uloží bajt do cipher na index 
+continue_under:	
+	addi r23, r23, 1   	; pointer increment
+	addi r20, r20, 1   	; pointer increment
+;#################################
+;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	j loop	
+
+
+overflow_under:
+	addi r15, r0, 97
+	sub r15, r15, r19
+	
+	addi r19, r0, 122
+	sub r19, r19, r15	
+	sb cipher(r20), r19     ; uloží bajt do cipher na index 
+	j continue_under
+
+
+overflow_over:
+	addi r15, r0, 122
+	sub r19, r19, r15
+	addi r15, r0, 97	
+	add r19, r19, r15
+	sb cipher(r20), r19     ; uloží bajt do cipher na index 
+	j continue
+
+num:		
+	addi r19, r0, 0x00
+	sb cipher(r20), r19     ; uloží bajt do cipher na index 0
 
 
 end: 	addi r14, r0, caddr 	; <-- pro vypis sifry nahradte laddr adresou caddr
@@ -76,14 +128,3 @@ end: 	addi r14, r0, caddr 	; <-- pro vypis sifry nahradte laddr adresou caddr
         trap 0  		; ukonceni simulace
 
 
-
-;Za návěštím cipher je vyhrazeno neinicializované místo pro
-;zašifrovaný text. Sem zapisujte zašifrované znaky. Za návěštím end
-;je připraven kód pro výpis uvítacího textu pomocí systémového
-;volání (trap), který změnte na výpis zašifrovaného textu. Pro
-;správnou funkci výpisu musí být řetězec ukončen hodnotou 0. Řiďte
-;se komentáři v textu a vaše řešení též přiměřeně komentujte.
-
-;Za návěští main zapište vaše řešení. Po dokončení soubor
-;vernam.s přejmenujte na váš login (se zachováním přípony .s)
-;a odevzdejte do IS FIT (bez zipování nebo jiných příloh).	
